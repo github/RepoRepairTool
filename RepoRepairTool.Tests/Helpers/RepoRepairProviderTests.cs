cb99e12a544a5dd8a9951852dca92fb13dc46d9b
Paul Betts <paul@paulbetts.org>
2012-05-29 17:52:38 -0700

    Stub out some things we want to test

---
+++ RepoRepairTool.Tests/Helpers/RepoRepairProviderTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Extensions;
+
+namespace RepoRepairTool.Tests.Helpers
+{
+    public class RepoRepairProviderTests
+    {
+        [Fact]
+        public void BrokenRemoteBranchesNeedTrackingBranches()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void CleanRepositoryShouldntBeChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory]
+        public void IntegrationFixturesShouldHaveACleanAnalysisReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
