5de0137756eea8e65b67c9861275537ba2c5eb4b
Paul Betts <paul@paulbetts.org>
2012-05-29 18:10:11 -0700

    Write some beautiful code to extract repo fixtures

--- RepoRepairTool.Tests/IntegrationTestHelper.cs
+++ RepoRepairTool.Tests/IntegrationTestHelper.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reactive.Disposables;
+using GitHub;
+using Ionic.Zip;
+using LibGit2Sharp;
+using RepoRepairTool.Helpers;
+using Should;
 
 namespace RepoRepairTool.Tests
 {
@@ -23,6 +29,30 @@ namespace RepoRepairTool.Tests
             var di = new DirectoryInfo(Path.GetDirectoryName(st.GetFileName()));
 
             return di.FullName;
+        }
+
+        public static DisposableContainer<string> GetEmptyDirectory()
+        {
+            var di = new DirectoryInfo(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()));
+            di.Create();
+
+            return DisposableContainer.Create(di.FullName, CoreUtility.DeleteDirectory);
+        }
+
+        public static DisposableContainer<Repository> GetRepositoryFromFixture(string fixtureName)
+        {
+            var zipPath = GetPath("fixture", fixtureName + ".zip");
+            var target = GetEmptyDirectory();
+
+            File.Exists(zipPath).ShouldBeTrue();
+
+            using (var zf = new ZipFile(zipPath)) {
+                zf.ExtractAll(target.Value, ExtractExistingFileAction.Throw);
+            }
+
+            CoreUtility.ExtractLibGit2();
+            var ret = new Repository(Path.Combine(target.Value, fixtureName));
+            return DisposableContainer.Create(ret, new CompositeDisposable(ret, target));
         }
 
         public static bool SkipTestOnXPAndVista()
---
+++ RepoRepairTool/Helpers/DisposableContainer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading;
+
+namespace RepoRepairTool.Helpers
+{
+    public static class DisposableContainer
+    {
+        public static DisposableContainer<T1> Create<T1>(T1 value, IDisposable disposable)
+        {
+            return new DisposableContainer<T1>(value, disposable);
+        }
+
+        public static DisposableContainer<T1> Create<T1>(T1 value, Action<T1> disposable)
+        {
+            return new DisposableContainer<T1>(value, Disposable.Create(() => disposable(value)));
+        }
+    }
+
+    public sealed class DisposableContainer<T> : IDisposable
+    {
+        IDisposable _inner;
+
+        public T Value { get; private set; }
+
+        public DisposableContainer(T value, IDisposable disposable)
+        {
+            Value = value;
+            _inner = disposable;
+        }
+
+        public void Dispose()
+        {
+            var disp = Interlocked.Exchange(ref _inner, null);
+            if (disp != null)
+            {
+                disp.Dispose();
+            }
+        }
+    }
+}
