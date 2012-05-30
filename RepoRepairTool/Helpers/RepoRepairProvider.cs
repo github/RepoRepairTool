f4549d0b9e5350767a04eebbfdec0836d283ba7e
Paul Betts <paul@paulbetts.org>
2012-05-30 05:01:13 +0200

    Implement the top-level method of RepairRepo

--- RepoRepairTool/Helpers/RepoRepairProvider.cs
+++ RepoRepairTool/Helpers/RepoRepairProvider.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
+using GitHub.Helpers;
 using LibGit2Sharp;
 using ReactiveUI;
 using RepoRepairTool.ViewModels;
@@ -8,19 +13,54 @@ namespace RepoRepairTool.Helpers
 {
     [Flags]
     public enum RepoRepairOptions {
-        IgnoreRemoteBranches = 0x1,
+        IgnoreRemoteBranches = 0x1 << 0,
+        IgnoreWorkingDirectory = 0x1 << 1,
     }
 
     public interface IRepoRepairProvider : IEnableLogger
     {
-        IObservable<Unit> RepairRepo(Repository repo, RepoAnalysisResult analysis, RepoRepairOptions ignoreRemoteBranches);
+        IObservable<Unit> RepairRepo(Repository repo, RepoAnalysisResult analysis, RepoRepairOptions options);
     }
 
     public class RepoRepairProvider : IRepoRepairProvider
     {
-        public IObservable<Unit> RepairRepo(Repository repo, RepoAnalysisResult analysis, RepoRepairOptions ignoreRemoteBranches)
+        public IObservable<Unit> RepairRepo(Repository repo, RepoAnalysisResult analysis, RepoRepairOptions options)
         {
-            throw new NotImplementedException();
+            // NB: Try to grab a bunch of locks beforehand, so that if some dumb
+            // program has files locked, we won't fail halfway through
+            IObservable<Unit> repairWd = Observable.Empty<Unit>();
+
+            if (!options.HasFlag(RepoRepairOptions.IgnoreWorkingDirectory)) {
+                IEnumerable<Tuple<string, Stream>> allWdFiles;
+
+                try {
+                    allWdFiles = repo.OpenAllFilesInWorkingDirectory(FileAccess.ReadWrite);
+                } catch (Exception ex) {
+                    return Observable.Throw<Unit>(ex);
+                }
+
+                repairWd = Observable.Defer(() =>
+                    repairWorkingDir(allWdFiles, analysis.BranchAnalysisResults[Constants.WorkingDirectory]));
+            }
+
+            return repo.ProcessAllBranchesAsync(branch => repairBranch(branch, analysis.BranchAnalysisResults[branch.Name], options))
+                .Select(_ => Unit.Default)
+                .Merge(repairWd)
+                .Aggregate(Unit.Default, (acc, _) => acc);
+        }
+
+        IObservable<Unit> repairBranch(Branch branch, HeuristicTreeInformation branchAnalysis, RepoRepairOptions options)
+        {
+            if (options.HasFlag(RepoRepairOptions.IgnoreRemoteBranches) && branch.IsRemote && !branch.IsTracking) {
+                return Observable.Return(Unit.Default);
+            }
+
+            return Observable.Throw<Unit>(new NotImplementedException());
+        }
+
+        IObservable<Unit> repairWorkingDir(IEnumerable<Tuple<string, Stream>> filesInWd, HeuristicTreeInformation dirAnalysis)
+        {
+            return Observable.Throw<Unit>(new NotImplementedException());
         }
     }
 }
